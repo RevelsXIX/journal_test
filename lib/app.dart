897d38085184a528a975a1e98f41214cfac9dab6
RevelsXIX <joshua.revels@gmail.com>
2022-02-14 16:13:04 -0500

    Second Commit

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'My Journal',
+        // theme: ThemeData(primarySwatch: Colors.red),
+        home: Scaffold(
+            appBar: AppBar(
+                title: Text('My Journal')
+            )
+        )
+    );
+  }
+}
+
